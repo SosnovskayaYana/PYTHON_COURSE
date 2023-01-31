6bd04b36109579c023557936fae790bfcdb1ad26
Yana <yan4ik999@yandex.ru>
2023-01-31 21:14:30 +0300

    task1

--- дз питон/DZ_2/task_01.py
+++ дз питон/DZ_2/task_01.py
@@ -5,3 +5,28 @@
 # Выведите минимальное количество монет, которые нужно перевернуть.
 # Пример:
 # 5 -> 1 0 1 1 0
+
+coins = int(input('введите количество монет: '))
+coins_list = []
+
+for i in range(coins):
+    position = int(input('введите значение: орел -> "0" или решка -> "1": '))
+    coins_list.append(position)
+# print(coins_list)
+
+count_0 = 0
+count_1 = 0
+
+for i in range(len(coins_list)):
+    if coins_list[i] == 0:
+        count_0 += 1
+    else:
+        count_1 += 1
+# print(f'0 -> {count_0}, 1 -> {count_1}')
+
+if count_1 < count_0:
+    print(f'Нужно перевернуть {count_1} монет ')
+elif count_0 < count_1:
+    print(f'Нужно перевернуть {count_0} монет ')
+elif count_0 == count_1:
+    print(f'Нужно перевернуть {count_0} монет ')
