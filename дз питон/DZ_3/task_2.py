08665e5eace70f6e28bd2fb2c9d38fb539de14ed
Yana <yan4ik999@yandex.ru>
2023-02-05 12:46:16 +0300

    task2

--- дз питон/DZ_3/task_2.py
+++ дз питон/DZ_3/task_2.py
@@ -9,3 +9,40 @@
 # 1 2 3 4 5
 # 6
 # -> 5
+
+n = int(input('введите количество элементов: '))
+a = []
+
+for i in range(n):
+    elem = int(input('введите элемент: '))
+    a.append(elem)
+print(a)
+
+x = int(input('введите X: '))
+
+# a = [2, -5, 7, 6, 20]
+# x = -30
+
+res = 0
+res_elem = 999999999
+x_element = 0
+
+for i in range(len(a)):
+
+    if a[i] <= x:
+        res = x - a[i]
+        if res < res_elem:
+            res_elem = res
+            x_element = a[i]
+        else:
+            i += 1
+
+    elif a[i] > x:
+        res = a[i] - x
+        if res < res_elem:
+            res_elem = res
+            x_element = a[i]
+        else:
+            i += 1
+
+print(f'Самый близкий по величине элемент к заданному X: {x_element}')
