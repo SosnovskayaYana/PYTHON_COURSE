1460a29337064d64e681f2044cdb23b6f6d4eade
Yana <yan4ik999@yandex.ru>
2023-02-09 15:17:57 +0300

    lection_recurcion

--- семинары/main.py
+++ семинары/main.py
@@ -1,3 +1,10 @@
-import modul
+def fib(n):
+    if n in [1, 2]:
+        return 1
+    return fib(n-1) + fib(n-2)
 
-print(modul.max1(10, 3))
+list1 = []
+
+for i in range(1, 10):
+    list1.append(fib(i))
+print(list1)
--- семинары/заметки.py
+++ семинары/заметки.py
@@ -61,3 +61,60 @@
 #         print('игра окончена ')
 #         break
 # print('Спасибо за игру ')
+
+
+
+# Фибаначчи. Рекурсия
+# def fib(n):
+#     if n in [1, 2]:
+#         return 1
+#     return fib(n-1) + fib(n-2)
+# list1 = []
+# for i in range(1, 10):
+#     list1.append(fib(i))
+# print(list1)
+
+
+
+# Быстрая сортировка
+# def quicksort(array):
+#     if len(array) < 2:
+#         return array
+#     else:
+#         pivot = array[0]
+#         less = [i for i in array[1:] if i <= pivot]
+#         greater = [i for i in array[1:] if i > pivot]
+#         return quicksort(less) + [pivot] + quicksort(greater)
+# print(quicksort([10, 5, 2, 3]))
+
+
+
+# Сортировка слиянием
+# def merge_sort(nums):
+#     if len(nums) > 1:
+#         mid = len(nums) // 2
+#         left = nums[:mid]
+#         right = nums[mid:]
+#         merge_sort(left)
+#         merge_sort(right)
+#         i = j = k = 0
+#         while i < len(left) and j < len(right):
+#             if left[i] < right[j]:
+#                 nums[k] = left[i]
+#                 i += 1
+#             else:
+#                 nums[k] = right[j]
+#                 j += 1
+#             k += 1
+#         while i < len(left):
+#             nums[k] = left[i]
+#             i += 1
+#             k += 1
+#         while j < len(right):
+#             nums[k] = right[j]
+#             j += 1
+#             k += 1
+
+# list1 = [3, 6, 5, 1, 2, 3, 40, 49, 87, 32, 14]
+# merge_sort(list1)
+# print(list1)
