e8c6cf53e232dd23954dc08118aee1a0f9a9c9c8
Yana <yan4ik999@yandex.ru>
2023-03-14 01:53:30 +0300

    phone_book

--- семинары/seminar_7.py
+++ семинары/seminar_7.py
@@ -1,21 +1,21 @@
 # # создать копию имеющегося списка
 
-# values = [1, 4, 5, 'asf', 2.323, True]
-# result = list(map(lambda list_item: list_item, values))
+values = [1, 4, 5, 'asf', 2.323, True]
+result = list(map(lambda list_item: list_item, values))
 
-# if result == values:
-#     print('ok')
-# else:
-#     print('not ok')
+if result == values:
+    print('ok')
+else:
+    print('not ok')
 
 
 
-result = [1, 5, 7, 3, 7]
-new_list_1 = list(map(lambda num: num*num, result))
-# что сделать где взять при каком условии  list conprehencion
-new_list_2 = [item*item for item in result if item > 4]
-print(new_list_1)
-print(new_list_2)
+# result = [1, 5, 7, 3, 7]
+# new_list_1 = list(map(lambda num: num*num, result))
+# # что сделать где взять при каком условии  list conprehencion
+# new_list_2 = [item*item for item in result if item > 4]
+# print(new_list_1)
+# print(new_list_2)
 
 
 
--- семинары/seminar_8/phone_book.py
+++ семинары/seminar_8/phone_book.py
@@ -10,64 +10,107 @@
 # 3. Добавить контакт (сразу сохрорнять в файл)
 # 4. Выход по требованию пользователя
 
-# Иванов Иван +79165610080
-# Смирнов Сергей +79165610081
-# Мосина Ирина +79165610082
-# Пушкин Александр +79165610083
-# Кашина Светлана +79165610084
-# Елесеев Федор +79165610085
-# Пушкин Сергей +79165610086
-# Есина Татьяна +79165610087
-# Есин Олег +79165610088
-# Тарасова Ольга +79165610089
+
+text_file = 'семинары/seminar_8/numbers.txt'  # путь к текстовому файлу указан в переменной
+
 
 def all_contacts():
-    with open('семинары/seminar_8/numbers.txt', 'r', encoding='utf8') as data:
+    with open(text_file, 'r', encoding='utf8') as data:
         for line in data:
             print(line)
-
 # all_contacts()
 
 
 def find_contact(contact):
     # contact = contact.upper()
-    with open('семинары/seminar_8/numbers.txt', 'r', encoding='utf8') as data:
+    with open(text_file, 'r', encoding='utf8') as data:
         for line in data:
             if contact in line:
                 print(line)
             # else:
             #     print("Контакт не найден ")
-
 # find_contact('')
 
 
 def add_contact(contact):
-    with open('семинары/seminar_8/numbers.txt', 'a', encoding='utf8') as data:
+    with open(text_file, 'a', encoding='utf8') as data:
         # data.write('\n')
         # data.write(contact)
         data.write('\n' + contact)
         print("Контакт сохранен")
-
 # add_contact('')
 
 
+def del_contact(contact):
+    with open(text_file, 'r', encoding='utf8') as data:
+        new_text = []
+        for line in data:
+            if contact not in line:
+                new_text.append(line)
+            else:
+                continue
 
-def main_menu(numb):
-    if numb == 1:
+    with open(text_file, 'w', encoding='utf8') as data:
+        data.writelines(new_text)
+        print('Данные удалены ')   
+# del_contact('СЕРГЕЙ')
+
+
+def change_contact(contact):
+    with open(text_file, 'r', encoding='utf8') as data:
+        new_text = []
+        for line in data:
+            if contact in line:
+                x = input(f"{line} -> Введите новые данные или прежние: ").upper()
+                new_text.append(x + '\n')
+            else:
+                new_text.append(line)
+    
+    with open('семинары/seminar_8/numbers.txt', 'w', encoding='utf8') as data:
+        data.writelines(new_text)
+        print('Данные изменены ')
+# change_contact('ОЛЕГ')
+
+
+def main_menu(number):
+    if number == 1:
         all_contacts()
-    elif numb == 2:
-        name = input("Введите имя: ")
+    elif number == 2:
+        name = input("Введите данные для поиска: ")
         find_contact(name.upper())
-    elif numb == 3:
+    elif number == 3:
         data = input("Введите новый контакт: ")
         add_contact(data.upper())
+    elif number == 5:
+        data = input("Введите данные для удаления: ")
+        del_contact(data.upper())
+    elif number == 6:
+        data = input("Введите данные для изменения: ")
+        change_contact(data.upper())
 
 while True:
-    numb = int(input('Введите 1 - для печати всего справочника;'
-                ' 2 - для поиска контакта;'
-                ' 3 - для записи контакта;'
-                ' 4 - для завершения:'))
-    if numb == 4:
+    number = int(input('Введите:\n'
+                ' 1 - для печати всего справочника;\n'
+                ' 2 - для поиска контакта;\n'
+                ' 3 - для записи контакта;\n'
+                ' 4 - для завершения программы;\n'
+                ' 5 - для удаления контакта;\n'
+                ' 6 - для изменения контакта;\n'
+                ' ->  '))
+    if number == 4:
         break
-    main_menu(numb)
+    main_menu(number)
+
+
+# def copy_contacts():
+#     with open(text_file, 'r', encoding='utf8') as data:
+#         new_text = []
+#         for line in data:
+#             new_text.append(line)
+#         return new_text
+
+# with open('семинары/seminar_8/phones.txt', 'w', encoding='utf8') as data:
+#     data.writelines(copy_contacts())
+#     print('ok')
+
 
